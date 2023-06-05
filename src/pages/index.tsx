3228d5c7649e3c714f91fd8373f186e49ca77b4c
MaikeruDev <hubufanlp@gmail.com>
2023-06-05 15:43:23 +0200

    ses

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,38 @@ import Navbar from '../components/Navbar';
 
 const Layout = ({ children }) => {
   return (
-    <div className="flex min-h-screen"> 
-      <Sidebar />
-      <Navbar />
-      <main className="flex-1 bg-white">{children}</main>
+    <div>
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <meta name="author" content="Michael Prietl" />
+        <meta name="description" content="Welcome to the personal website of Michael Prietl, also known as MaikeruDev. Get insights into my projects, skills, and experiences as a developer." />
+  
+        <meta property="og:title" content="Michael Prietl | MaikeruDev - Personal Website" />
+        <meta property="og:description" content="Welcome to the personal website of Michael Prietl, also known as MaikeruDev. Get insights into my projects, skills, and experiences as a developer." />
+        <meta property="og:url" content="https://maikeru.dev" />
+        <meta property="og:type" content="website" />
+
+        <meta name="language" content="English" />
+
+        <meta name="keywords" content="personal website, developer, projects, skills, experiences" />
+
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black"/> 
+ 
+        <meta name="twitter:site" content="@MaikeruDev" />
+        
+        <meta name="msapplication-TileColor" content="#ffffff" />
+      </head>
+      <div className="flex min-h-screen"> 
+        <Sidebar />
+        <Navbar />
+        <main className="flex-1 bg-white">{children}</main>
+      </div>
     </div>
-  );
-};
+  ); 
+}
 
 export default Layout;
